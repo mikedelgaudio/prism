3235b014facf743b3a5058c8d1c83f5b921fdac1
Mike DelGaudio <delgaudiomike@gmail.com>
2023-01-04 18:20:03 -0600

    test commit

--- frontend/src/services/util.service.ts
+++ frontend/src/services/util.service.ts
@@ -2,6 +2,5 @@ export const validString = (string: string | undefined): boolean => {
   if (!string) return false;
   if (string.length === 0) return false;
   if (!string.trim().length) return false;
-
   return true;
 };
