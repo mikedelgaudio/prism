b9494506756141437ef5044c1258d98101cc6445
Mike DelGaudio <delgaudiomike@gmail.com>
2023-01-31 15:35:58 -0500

    YAGNI

--- frontend/src/components/Shared/index.ts
+++ frontend/src/components/Shared/index.ts
@@ -1,4 +1,3 @@
 export { AuthLayout } from "./auth-layout.component";
 export { Card } from "./card.component";
 export { Loading } from "./loading.component";
-export { Modal } from "./modal.component";
