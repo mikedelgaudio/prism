40a3c57cbcd78232a7a0ed6282dc543815dbd029
Mike DelGaudio <delgaudiomike@gmail.com>
2023-02-03 18:53:16 -0500

    adding in api service

--- backend/src/middleware/firebase.middleware.ts
+++ backend/src/middleware/firebase.middleware.ts
@@ -1,21 +1,17 @@
 import type { NextFunction, Request, Response } from "express";
 import { admin } from "../config/firebase.config";
 
-export interface IAuthReq extends Request {
-  headers: any;
-}
-
 export const checkAuth = async (
-  req: IAuthReq,
+  req: Request,
   res: Response,
   next: NextFunction,
 ): Promise<any> => {
   try {
-    if (req.headers.token !== undefined) throw new Error("Unauthorized");
-    const token = req.headers.token;
+    if (req.headers.token === undefined) throw new Error("Unauthorized");
+    const token = req.headers.token as string;
     await admin.auth().verifyIdToken(token);
     next();
   } catch (e) {
-    res.status(403).json(e);
+    res.status(403).json({ error: "Unauthorized" });
   }
 };
