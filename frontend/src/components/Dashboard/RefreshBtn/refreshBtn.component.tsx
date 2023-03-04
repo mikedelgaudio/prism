4c7bbac6b7aa1a4416a3e0d9e596b28e0a084ef6
Mike DelGaudio <delgaudiomike@gmail.com>
2023-03-04 11:00:12 -0600

    fix graph size

--- frontend/src/components/Dashboard/RefreshBtn/refreshBtn.component.tsx
+++ frontend/src/components/Dashboard/RefreshBtn/refreshBtn.component.tsx
@@ -36,14 +36,14 @@ const RefreshBtn = observer(({ iconMode = false }: { iconMode: boolean }) => {
       />
     </svg>
   ) : (
-    <>Request new data</>
+    <>Reload data</>
   );
 
   return (
     <button
       className="flex items-center justify-center rounded-xl border border-slate-900 py-1 px-2 text-base font-semibold leading-7 transition-all duration-200 hover:bg-transparent hover:bg-slate-900 hover:text-white focus:bg-transparent focus:text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-700 disabled:text-white"
       disabled={query.isFetching}
-      onClick={() => query.refetch()}
+      onClick={() => window.location.reload()}
     >
       {query.isFetching ? "Loading..." : message}
     </button>
