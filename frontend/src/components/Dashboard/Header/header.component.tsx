0d75d42dd28a4d289d42f08247b7b1e2ac756d44
Mike DelGaudio <delgaudiomike@gmail.com>
2023-04-05 08:14:36 -0700

    update styling for header of settings and greetings to user

--- frontend/src/components/Dashboard/Header/header.component.tsx
+++ frontend/src/components/Dashboard/Header/header.component.tsx
@@ -2,12 +2,16 @@ import { observer } from "mobx-react";
 import { useContext } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { FirebaseContextNew } from "../../../firebase/firebase.context.new";
+import { randomIntFromInterval } from "../../../services/util.service";
 
 const Header = observer(() => {
   const { pathname } = useLocation();
   const weekView = pathname === "/dashboard/week";
 
   const { firebaseStore } = useContext(FirebaseContextNew);
+
+  const greetings = ["Great to see you", "Welcome", "Welcome back"];
+  const rndInt = randomIntFromInterval(0, 2);
 
   const btnStyle =
     "flex items-center justify-center rounded-xl border border-slate-900 px-5 py-3 text-base lg:text-xl font-semibold leading-7 transition-all duration-200 hover:bg-slate-900 hover:text-white focus:bg-transparent focus:text-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2";
@@ -22,15 +26,11 @@ const Header = observer(() => {
           <h1 className="text-4xl font-bold leading-tight text-slate-900 sm:text-5xl sm:leading-tight lg:text-5xl lg:leading-tight">
             {weekView
               ? "Week review"
-              : `Great to see you ${firebaseStore.authUser?.displayName ?? ""}`}
+              : `${greetings[rndInt]} ${
+                  firebaseStore.authUser?.displayName ?? ""
+                }`}
           </h1>
         </div>
-
-        {weekView ? (
-          <p className="pl-1">Track your improvement</p>
-        ) : (
-          <p className="pl-1">Here's how you're doing daily</p>
-        )}
       </div>
       <div className="flex gap-4 justify-center md:justify-start">
         <NavLink
--- frontend/src/services/util.service.ts
+++ frontend/src/services/util.service.ts
@@ -94,3 +94,8 @@ export const getWeekNumber = (date?: string): number => {
   const weekNumber = Math.ceil(days / 7);
   return weekNumber;
 };
+
+export function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
